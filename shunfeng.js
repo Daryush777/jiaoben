4822c51d671b72611199bab4759c43b7d9338955
Daryush777 <117909785+Daryush777@users.noreply.github.com>
2023-10-15 01:51:09 +0800

    更新 shunfeng.js

--- shunfeng.js
+++ shunfeng.js
@@ -1,7 +1,8 @@
-顺丰签到
-https://raw.githubusercontent.com/chavyleung/scripts/master/sfexpress/sfexpress.js
-
-本地重写获取cookie
+[rewrite_local]
 ^https:\/\/ccsp-egmas.sf-express.com\/cx-app-member\/member\/app\/user\/universalSign url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/sfexpress/sfexpress.cookie.js
 
-ccsp-egmas.sf-express.com
+[task_local]
+37 8 * * * https://raw.githubusercontent.com/chavyleung/scripts/master/sfexpress/sfexpress.js, tag=顺丰, enabled=true
+
+[mitm]
+hostname = ccsp-egmas.sf-express.com
