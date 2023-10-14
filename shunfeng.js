7567893f33cce1dbc8178c1b0e39c3a1e46448c2
Daryush777 <117909785+Daryush777@users.noreply.github.com>
2023-10-15 02:59:06 +0800

    更新 shunfeng.js

--- shunfeng.js
+++ shunfeng.js
@@ -2,5 +2,5 @@
 hostname = ccsp-egmas.sf-express.com
 
 [Script]
-http-request ^https:\/\/ccsp-egmas.sf-express.com\/cx-app-member\/member\/app\/user\/universalSign script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/sfexpress/sfexpress.cookie.js, requires-body=true, tag=Rewrite: 顺丰速运
+Rewrite: 顺丰速运 = type=http-request, pattern=^https:\/\/ccsp-egmas.sf-express.com\/cx-app-member\/member\/app\/user\/universalSign,script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/sfexpress/sfexpress.cookie.js,requires-body=true
 cron "1 0 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/sfexpress/sfexpress.js
